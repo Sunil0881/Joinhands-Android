4fb2bf160fd7a98c7bb6ee2ba405d67e596e4840
Sunil_0881 <sunil12162003@gmail.com>
2024-02-04 17:08:49 +0530

    f

---
+++ Joinhands/Donorscreens/donorhome.js
@@ -0,0 +1,20 @@
+import { styled } from 'nativewind';
+import React from 'react';
+import {  Text, TouchableOpacity, Button, TextInput, View, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { ImageBackground } from 'react-native';
+
+const  StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledTextInput = styled(TextInput);
+
+
+const Donorhome = () => {
+    return ( 
+        <StyledView>
+            <StyledText>hello home</StyledText>
+        </StyledView>
+     );
+}
+ 
+export default Donorhome;
